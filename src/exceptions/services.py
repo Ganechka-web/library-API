84ec8680f71ec4ef07d4271e302adc716ad468dc
Ganechka <maksim15848@gmail.com>
2025-05-23 12:51:26 +0300

    feat(exceptions): BookDoesNotExist exception

--- src/exceptions/services.py
+++ src/exceptions/services.py
@@ -4,10 +4,16 @@ from exceptions.base import BaseError
 class ServiceError(BaseError):
     pass
 
+### auth exceptions ### 
 
 class UserDoesNotExist(ServiceError):
     pass
 
 
 class UserPasswordVerificationFailedError(ServiceError):
+    pass
+
+### book exceptions ###
+
+class BookDoesNotExist(ServiceError):
     pass
