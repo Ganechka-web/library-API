cbfe26ee094a8f87bcdb0c1c4f2b459f147118e8
Ganechka <maksim15848@gmail.com>
2025-05-26 02:45:36 +0300

    feat(exceptions): borrowed_book exceptions, BookDoesNotHaveAnyInstancesError

--- src/exceptions/services.py
+++ src/exceptions/services.py
@@ -22,6 +22,10 @@ class BookDoesNotExist(ServiceError):
 class BookISBNAlreadyExists(BookDoesNotExist):
     pass
 
+
+class BookDoesNotHaveAnyInstancesError(ServiceError):
+    """raises when reader trying to borrow book with 0 instances"""
+
 ### reader exceptions ###
 
 class ReaderDoesNotExist(ServiceError):
@@ -30,3 +34,25 @@ class ReaderDoesNotExist(ServiceError):
 
 class ReaderEmailAlreadyExists(ServiceError):
     pass
+
+### borrowed_book exceptions ###
+
+class BorrowedBookDoesNotExist(ServiceError):
+    pass
+
+
+class BorrowedBookAlreadyBorrowed(ServiceError):
+    pass
+
+
+class BorrowedBookCountPerReaderError(ServiceError):
+    """raises when reader trying to borrow fouth book"""
+
+
+class BorrowedBookInvalidReturnDateError(ServiceError):
+    """raises when return_at is less than borrow_at date"""
+
+
+class BorrowedBookUnableToBorrowBook(ServiceError):
+    """raises when book instances is 0"""
+    
