d914e404131b6366831f60a22bcc8bcfc36d8a45
Ganechka <maksim15848@gmail.com>
2025-05-26 02:49:26 +0300

    feat(main.py): add borrowed_book router

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from api.endpoints.auth import auth_router
 from api.endpoints.book import book_router
 from api.endpoints.reader import reader_router
+from api.endpoints.borrowed_book import borrowed_book_router
 
 
 app = FastAPI(
@@ -13,3 +14,4 @@ app = FastAPI(
 app.include_router(auth_router)
 app.include_router(book_router)
 app.include_router(reader_router)
+app.include_router(borrowed_book_router)
